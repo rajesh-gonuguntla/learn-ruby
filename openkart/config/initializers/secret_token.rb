0dbb9a88ce170fe3cd27f4aa8e72cbbe2b5ac145
Rajesh <g.rajeshchowdary@gmail.com>
2013-09-11 17:31:16 +0530

    initial copy

---
+++ openkart/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openkart::Application.config.secret_token = '1c179dbc5dfb8315d8349a43c5c2f2468bc8637136582918bf7404105f469b6057eefe47724abade54db463ec4eed5ace9445b86dfe03635bc74eeef5477efb3'
